b877f166543201676d9c35ba69bf7b02f31b4bf2
Ivan Dimitrov <idimitrov07@gmail.com>
2015-04-04 12:43:47 +0300

    Add destroy, update actions. Move form for create and edit to partial.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,12 @@
 class ArticlesController < ApplicationController
 	include ArticlesHelper
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@articles = Article.all
 	end
 
 	def show
-		@article = Article.find(params[:id])
 	end
 
 	def new
@@ -16,6 +16,21 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 		@article.save
+		redirect_to articles_path
+	end
+
+	def edit
+	end
+
+	def update
+		@article.update(article_params)
+		logger.debug "Post was updated..."
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article.delete
+		logger.debug "Post was deleted and now redirect.."
 		redirect_to articles_path
 	end
 
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,5 +3,9 @@ module ArticlesHelper
 	def article_params
 		params.require(:article).permit(:title, :body)
 	end
-	
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
 end
