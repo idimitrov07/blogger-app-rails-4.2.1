2b2f47d8a80cd826e2dd6160a320fc551992cff7
Ivan Dimitrov <idimitrov07@gmail.com>
2015-04-08 21:28:31 +0300

    Add extract hashtags from body text and add them to article.

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,17 @@ class Article < ActiveRecord::Base
 
   scope :desc,->{ order("id DESC") }
 
+  before_save :set_tags
+
+
+
+
+  protected
+  def set_tags
+    tag_names = body.downcase.split.find_all{|word| /^#.+/.match word}.uniq
+    tag_names = tag_names.map { |x| x[1..-1] }
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
+
 end
