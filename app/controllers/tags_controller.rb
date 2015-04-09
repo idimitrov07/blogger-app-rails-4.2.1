e233b85f6119e6bd5e5da440061b859690a24bf4
Ivan Dimitrov <idimitrov07@gmail.com>
2015-04-09 09:12:20 +0300

    Add index and destroy methods for tags.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,4 +3,15 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.delete
+    flash.notice = "Tag '#{@tag.name}' deleted!"
+    redirect_to tags_path
+  end
 end
