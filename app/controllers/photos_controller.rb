ec97ac7a14b886b48beab2745d27048b249c43d3
Ivan Dimitrov <idimitrov07@gmail.com>
2015-04-10 19:48:23 +0300

    Add view for creating new photos. Move hashtag func to app.js

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,21 @@ class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.save
+    flash.notice = "You added a photo '#{@photo.title}'!"
+    redirect_to photos_path
+  end
+
+
+  private
+  def photo_params
+    params.require(:photo).permit(:title, :body)
+  end
 end
